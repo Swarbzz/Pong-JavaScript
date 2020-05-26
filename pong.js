3d1e79e8ae7f7c0e275a37fd27d1f0022baa8503
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-05-26 15:09:30 +0100

    add ball position, size and velocity

--- pong.js
+++ pong.js
@@ -7,8 +7,29 @@ class Vec
   }
 }
 
+class Rect
+{
+  constructor(w, h)
+  {
+    this.pos = new Vec;
+    this.size = new Vec(w, h);
+  }
+}
+
+class Ball extends Rect
+{
+  constructor()
+  {
+    super(10, 10);
+    this.vel = new Vec;
+  }
+}
+
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
+
+const ball = new Ball;
+console.log(ball);
 
 context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
